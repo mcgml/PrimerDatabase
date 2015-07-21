baf4efde5f7c550677ce394f912624bc2d4f7d89
msl1y11 <mlyon@live.co.uk>
2015-07-21 16:20:40 +0100

    devel

--- public/app/admin/query.js
+++ public/app/admin/query.js
@@ -8,35 +8,126 @@
 
         var getLogFn = common.logger.getLogFn;
         var log = getLogFn(controllerId);
+        var logError = getLogFn(controllerId, 'error');
+        var dateEpoch = new Date(0);
 
         var vm = this;
-        vm.title = 'Ad-hoc Queries';
-        vm.nodes = [];
-        vm.sampleQueries = [
-            {name: 'Show all drugs',
-            query: ['Match (n)',
-                   'Return n'
-                   ].join('\n')
-            },
-            {name: 'Show all relationships',
-            query: ['Match n-[r]->m',
-                   'Return n,r,m'
-                   ].join('\n')},
-            
-        ];
-        vm.runQuery = runQuery;
-        
+
+        vm.primerNameNodes = [];
+        vm.primerSequenceNodes = [];
+        vm.regionofInterestNodes = [];
+        vm.assayInfoExpanded = [];
+        vm.primerSequenceInfoExpanded = [];
+
+        vm.addCheckedBy = addCheckedBy;
+        vm.runPrimerNameQuery = runPrimerNameQuery;
+        vm.runPrimerSequenceQuery = runPrimerSequenceQuery;
+        vm.checkCypherLog = checkCypherLog;
+        vm.runTargetRegionQuery = runTargetRegionQuery;
+
         activate();
 
         function activate() {
             var promises = [];
             common.activateController(promises, controllerId)
-                .then(function () { log('Activated Ad-hoc Query View'); });
+                .then(function () {});
         }
 
-        function runQuery() {
-            return datacontext.runAdhocQuery(vm.query).then(function (result) {
-                return vm.nodes = result.data;
+        function checkCypherLog(){
+            
+            //check result
+            if (vm.neo4jReturn["error"] != null){
+
+                var error = vm.neo4jReturn["error"];
+                var innerError = error["innerError"];
+                var errorMessage = innerError["message"]
+
+                logError(errorMessage);
+                logError("Operation unsucessful");
+
+            } else {
+
+                var responseData = vm.neo4jReturn[responseData];
+                
+                for (var r in responseData){
+                    log(r + ":" + responseData[r]);
+                }
+
+                log("Operation successful");
+            }
+
+        }
+
+        function addCheckedBy(){
+        }
+        
+        function runPrimerNameQuery() {
+            
+            if (vm.primerName == undefined || vm.primerName == ""){
+                logError('Enter a primer name');
+                return;
+            }
+
+            return datacontext.runAdhocQuery("MATCH (order:Order {PrimerName:\"" + vm.primerName + "\"})<-[:HAS_ORDER]-(primer:Primer) return order, primer;").then(function (result) {
+                vm.neo4jReturn = result.data;
+                checkCypherLog();
+                return vm.primerNameNodes = result.data;
+            });
+        }
+
+        function runPrimerSequenceQuery() {
+            
+            if (vm.primerSequence == undefined || vm.primerSequence == ""){
+                logError('Enter a primer sequence');
+                return;
+            }
+
+        var query = "MATCH (primer:Primer {PrimerSequence:\"" + vm.primerSequence + "\"}) ";
+            query += "MATCH (primer)-[hasOrder:HAS_ORDER]->(order:Order) ";
+            query += "MATCH (primer)-[hasTarget:HAS_TARGET]->(assay:Assay) ";
+            query += "MATCH (order)-[hasLocation:HAS_LOCATION]->(storageLocation:StorageLocation) ";
+            query += "MATCH (order)-[orderedBy:ORDERED_BY]->(orderUser:User) ";
+            query += "MATCH (order)-[receivedBy:RECEIVED_BY]->(receiveUser:User) ";
+            query += "return primer, receiveUser, orderUser, hasOrder, order;";
+
+            return datacontext.runAdhocQuery(query).then(function (result) {
+                vm.neo4jReturn = result.data;
+                checkCypherLog();
+                return vm.primerSequenceNodes = result.data;
+            });
+        }
+
+        function runTargetRegionQuery() {
+
+            if (vm.targetRegion == undefined || vm.targetRegion == ""){
+                logError('Enter a target region');
+                return;
+            }
+
+            var fields = vm.targetRegion.split(/:|-/);
+            var prefix = vm.targetRegion.substring(0, 3);
+
+            if (fields.length != 3){
+                logError('Target location format is chr:start-end');
+                return;
+            }
+
+            if (prefix == "chr"){
+                logError('Do not use the chr prefix');
+                return;
+            }
+
+            if (parseInt(fields[1]) > parseInt(fields[2])){
+                logError('Coordinates should be ascending');
+                return;
+            }
+
+            var query = "MATCH (leftPrimer:Primer)-[:HAS_TARGET]->(assay:Assay)<-[:HAS_TARGET]-(rightPrimer:Primer) where assay.Contig = toString(" + fields[0] + ") AND assay.StartPos <= toInt(" + fields[1] + ") AND assay.EndPos >= toInt(" + fields[0] + ") ";
+                query += "MATCH (assay)-[checker:CHECKED_BY]->(user:User) return leftPrimer, assay, rightPrimer, checker, user;";
+            return datacontext.runAdhocQuery(query).then(function (result) {
+                vm.neo4jReturn = result.data;
+                checkCypherLog();
+                return vm.regionofInterestNodes = result.data;
             });
         }
 
--- public/app/layout/shell.js
+++ public/app/layout/shell.js
@@ -25,7 +25,6 @@
         activate();
 
         function activate() {
-            logSuccess('Welcome', null, true);
             common.activateController([], controllerId);
         }
 
